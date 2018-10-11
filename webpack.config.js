b61b7cc21772fdbc26b882b01c8baad42b99fbf3
Brett Goss <goss.brett@gmail.com>
2018-10-11 13:56:41 -0700

    Add fallback path to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,8 @@ module.exports = {
   entry: './app/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    filename: 'index_bundle.js',
+    publicPath: '/',
   },
   resolve: {
     extensions: ['.js', '.jsx']
@@ -15,6 +16,9 @@ module.exports = {
       { test: /\.jsx?$/, use: 'babel-loader' },
       { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
     ]
+  },
+  devServer: {
+    historyApiFallback: true,
   },
   mode: 'development',
   plugins: [
