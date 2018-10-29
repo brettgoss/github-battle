a33dbd4d662436747780b9eef3000178e2c0c090
Brett Goss <goss.brett@gmail.com>
2018-10-29 12:43:32 -0700

    Set the results state upon receiving props from Github

--- app/components/Results.jsx
+++ app/components/Results.jsx
@@ -1,8 +1,20 @@
 var React = require('react');
+var Link = require('react-router-dom').Link;
 var queryString = require('query-string');
 var api = require('../utils/api');
 
 class Results extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      winner: null,
+      loser: null,
+      error: null,
+      loading: true,
+    }
+  }
 
   componentDidMount () {
     var players = queryString.parse(this.props.location.search);
@@ -10,11 +22,43 @@ class Results extends React.Component {
       players.playerOneName,
       players.playerTwoName,
     ]).then(function (results) {
-      console.log(results)
-    })
+      if (results === null) {
+        return this.setState(function (){
+          return {
+            error: 'Looks like there was an error. Check that both users exist on Github',
+            loading: false,
+          }
+        })
+      }
+
+      this.setState(function () {
+        return {
+          error: null,
+          winner: results[0],
+          loser: results[1],
+          loading: false,
+        }
+      })
+    }.bind(this));
   }
 
   render () {
+    var error = this.state.error;
+    var winner = this.state.winner;
+    var loser = this.state.loser;
+    var loading = this.state.loading;
+
+    if (loading === true) {
+      return <p>Loading...</p>;
+    }
+
+    if (error) {
+      return <div>
+          <p>{error}</p>
+          <Link to='/battle'>Reset</Link>
+        </div>
+    }
+
     return (
       <div>Results</div>
     )
